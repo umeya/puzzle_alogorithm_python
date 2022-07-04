504d3b0120a712648d8ed56f4c71094cb119643f
梅屋萬年堂 <umeya.mann@gmail.com>
2022-07-04 09:26:16 +0900

    number_puzzle_solver.py , ten_puzzle_solver.pyのコミット

---
+++ ch01/number_puzzle_solver.py
@@ -0,0 +1,141 @@
+# coding: utf-8
+"""
+「パズルで鍛えるアルゴリズム力」（大槻兼資　著、技術評論社）のテンパズルソルバー（24~25ページ）を
+梅屋萬年堂がpythonで書き直したものです。
+目標合計値targetとなる式の探索本体のクラスです。
+2022/05/25
+"""
+from fractions import Fraction as fr
+
+class NumberPuzzleSolver():
+	def solve(self, digits:str, taget:int, decode_reverse_politish_notatio:bool =True) -> list:
+		"""
+
+		:param digits: 使用する４個の数字
+		:param taget: 目標の合計値
+		:param decode_reverse_politish_notatio:逆ポーランド記法で式を作り探索しているが、
+		　　　　　　　　結果を通常の式に戻すときにTrue,逆ポーランド記法のままの時はFaulse
+		:return: 目標合計値となる式（文字列）のリスト
+		"""
+		results = []
+		n, e, ne = self.four_digits_pattern(digits)
+		for di in self.generate_repeated_perm_r_tings_from_e(n, e, 4):
+			d = [ne[i - 1] for i in di]
+			for op1 in '+-*/':
+				for op2 in '+-*/':
+					for op3 in '+-*/':
+						exps = ["".join([d[0], d[1], d[2], d[3], op1, op2, op3]),
+								"".join([d[0], d[1], d[2], op1, d[3], op2, op3]),
+								"".join([d[0], d[1], d[2], op1, op2, d[3], op3]),
+								"".join([d[0], d[1], op1, d[2], d[3], op2, op3]),
+								"".join([d[0], d[1], op1, d[2], op2, d[3], op3])]
+						for exp in exps:
+							if self.calc_poland(exp) == taget:
+								if decode_reverse_politish_notatio:
+									results.append(self.decode_poland(exp))
+								else:
+									results.append(exp)
+		return results
+
+	def calc_poland(self, exp: str) -> fr:
+		space = []
+		for c in exp:
+			if '0' <= c <= '9':
+				space.append(int(c))
+			else:
+				second = space.pop()
+				first = space.pop()
+				if c == '+':
+					space.append(first + second)
+				elif c == '-':
+					space.append(first - second)
+				elif c == '*':
+					space.append(first * second)
+				else:  # c is '/'
+					if second == 0:
+						return None
+					space.append(fr(first, second))
+		return space.pop()
+
+	def decode_poland(self, exp: str) -> str:
+		space = []
+		for c in exp:
+			if '0' <= c <= '9':
+				space.append(c)
+			else:
+				second = space.pop()
+				first = space.pop()
+				if c in '*/':
+					if len(first) > 1:
+						first = '(' + first + ')'
+					if len(second) > 1:
+						second = '(' + second + ')'
+				space.append(first + c + second)
+		return space.pop()
+
+	def four_digits_pattern(self, four_digits: str) -> tuple:
+		"""
+		:param four_digits: ４桁の数の文字列　この関数内部でソートされたリストにする
+							（例　"2113" は　['1','1','2','3'])
+		:return:tuple (n, e, ne)
+						n 数字の個数　（例　3）
+						e 各数字の個数　（例　[2,1,1])
+						ne 各数字　（例　['1', '2', '3']
+		"""
+		n, dd = 0, 0
+		e0, ne0 = ['dummy'], [0]
+		for d in sorted(four_digits):
+			if d != ne0[n]:
+				n += 1
+				e0.append(1)
+				ne0.append(d)
+			else:
+				e0[n] = e0[n] + 1
+		return (n, e0[1:], ne0[1:])
+
+	def generate_repeated_perm_r_tings_from_e(self, n: int, e: list, r: int) -> list:
+		"""
+		「組み合わせアルゴリズム」（千波一郎、サイエンス社）の重複順列生成アルゴリズムを参考にし作成した
+		重複順列生成ジェネレータです。
+
+		要素がXi個あるn種類のものから重複を許してr個取り出してできる順列
+		:param n: 種類数
+		:param x: e[0]　種類1の個数、e[1]　種類２の個数、…、e[n-1]　種類nの個数
+		:param r: 取り出す個数（順列の長さ）
+		:return: 生成された順列のリスト
+		"""
+		x = [1] + e + [0]
+		p = [0 for i in range(r + 1)]
+		k = 0
+		while True:
+			while k < r and x[p[k]] > 0:
+				x[p[k]] = x[p[k]] - 1
+				k += 1
+				p[k] = 1
+			if k == r and x[p[k]] > 0:
+				yield (p[1:])
+			while p[k] == n:
+				k -= 1
+				x[p[k]] = x[p[k]] + 1
+			p[k] = p[k] + 1
+			if k == 0:
+				return
+
+	def generate_4digits_number_for_ten_puzzle(self):
+		"""
+		:return: ４桁の数 d1 d2 d3 d4
+		           d1 <= d2 <= d3 <= d4
+		"""
+		for d1 in range(10):
+			for d2 in range(d1, 10):
+				for d3 in range(d2, 10):
+					for d4 in range(d3, 10):
+						d = str(d1) + str(d2) + str(d3) + str(d4)
+						yield d
+
+
+
+if __name__ == '__main__':
+	#  テキスト２５ページの例
+	solver = NumberPuzzleSolver()
+	print(solver.solve('3478', 10))
---
+++ ch01/ten_puzzle_solver.py
@@ -0,0 +1,81 @@
+# coding: utf-8
+"""
+「パズルで鍛えるアルゴリズム力」（大槻兼資　著、技術評論社）のテンパズルソルバー（24~25ページ）を
+梅屋萬年堂がpythonで書き直したものです。
+展パズルのGUI部分をTkinterで実装しているクラスです。
+2022/05/25
+"""
+import tkinter as tk
+from tkinter import ttk
+
+
+from number_puzzle_solver import NumberPuzzleSolver
+
+class TenPuzzleSolver(ttk.Frame):
+	def __init__(self, master:tk.Tk):
+		super().__init__(master)
+		
+		master.title('ten puzzule solver')
+		fr0 = ttk.Frame(self)
+		lbl01 = tk.Label(fr0, width=38, relief=tk.RIDGE, bd=5,
+						text="「パズルで鍛えるアルゴリズム力」（大槻兼資　著、技術評論社）\n"
+							+ "のテンパズルソルバー（24~25ページ）を\n"
+							+  "梅屋萬年堂がpythonで書き直したものです\n"
+							+  "                     2022/05")
+		fr1 = ttk.LabelFrame(self, text='入力してください')
+		lbl11 = ttk.Label(fr1,text='４桁の数字')
+		tcl_onValidate_digits = master.register(self.onValidate_digits)
+		entry_4digits = tk.Entry(fr1, width=7, validate='key', vcmd=(tcl_onValidate_digits, '%P'))
+		lbl12 = ttk.Label(fr1, text='合計の数値')
+		tcl_onValidate_target = master.register(self.onValidate_target)
+		entry_target = tk.Entry(fr1, width=7, validate='key', vcmd=(tcl_onValidate_target, '%P'))
+		fr2 = ttk.Frame(self)
+		btn_search = ttk.Button(fr2, text='検索開始'
+			, command=lambda: self.on_btn_search(entry_4digits.get(), entry_target.get(), lstbx_results))
+		fr3 = ttk.LabelFrame(self, text='検索結果')
+		lstbx_results = tk.Listbox(fr3, width=10)
+
+		self.grid(column=0, row=0, sticky=tk.NSEW, padx=3, pady=3)
+		master.grid_anchor(tk.CENTER)
+		fr0.grid(column=0, row=0, padx=3, pady=3, ipadx=10, ipady=10)
+		lbl01.grid(column=0, row=0, padx=10, pady=10, ipadx=10, ipady=10)
+		fr1.grid(column=0, row=1, padx=3, pady=3)
+		lbl11.grid(column=0, row=0, padx=3, pady=3)
+		entry_4digits.grid(column=1, row=0, padx=3, pady=3)
+		lbl12.grid(column=0, row=1, padx=3, pady=3)
+		entry_target.grid(column=1, row=1, padx=3, pady=3)
+		fr2.grid(column=0, row=2, padx=3, pady=3)
+		btn_search.grid(column=0, row=0, padx=3, pady=3)
+		fr3.grid(column=0, row=3, padx=3, pady=3)
+		lstbx_results.grid(column=0, row=0, padx=3, pady=3)
+
+	def onValidate_digits(self, P):
+		if P == '' or (P.isdigit() and len(P) <= 4):
+			return True
+		else:
+			return False
+
+	def onValidate_target(self, P):
+		if P == '' or (P.isdigit() and int(P) <= 9 * 9 * 9 * 9):
+			return True
+		else:
+			return False
+
+	def on_btn_search(self, four_digits:str, target:int, lstbx:tk.Listbox):
+		solver = NumberPuzzleSolver()
+		exps = solver.solve(four_digits, int(target))
+		lstbx.delete(0, tk.END)
+		if len(exps) == 0:
+			lstbx.insert(tk.END, "作れません")
+		else:
+			for exp in exps:
+				lstbx.insert(tk.END, exp)
+			lstbx.insert(tk.END, ' -----------')
+			lstbx.insert(tk.END, f'  {len(exps)} 通り')
+		del solver
+
+if __name__ == '__main__':
+	root = tk.Tk()
+	root.geometry('400x500')
+	app = TenPuzzleSolver(master=root)
+	app.mainloop()
